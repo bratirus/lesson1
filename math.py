a1eab9b1242840bdd77c63e9f0a2a25ac0bf92e5
bratirus <60543874+bratirus@users.noreply.github.com>
2020-02-13 11:27:32 +0800

    Add files via upload

---
+++ math.py
@@ -0,0 +1,89 @@
+#Задача 1
+a=0
+for i in range(1,6):
+    a=a+1
+    print(a,0)
+#Задача 2
+'''
+a=0
+for i in range(1,11):
+    b=int(input('введите число'))
+    if b==5:
+        a=a+1
+    else:
+        a=a
+print(a)
+#Задача 3
+
+a = 0
+for i in range(1,100):
+    a=a+i
+print(a)
+#Задача 4
+
+pr = 1
+for i in range(1,10):
+    pr= pr*i
+print(pr)
+
+#Задача 5
+a=392723448374
+while a>0:
+    print(a%10)
+    a=a//10
+
+#Задача 6
+b=0
+e=0
+a=123
+while a>0:
+    b=a%10
+    e=e+b
+    a=a//10
+print(e)
+
+#Задача 7
+b=0
+e=1
+a=123456
+while a>0:
+    b=a%10
+    e=e*b
+    a=a//10
+print(e)
+
+#Задача 8
+a = 123456780897565
+while a>0:
+    if a%10 == 5:
+        print('Yes')
+        break
+    a = a//10
+else:
+    print('No')
+
+#Задача 9
+a=12345678087565
+b=0
+while a>0:
+    if a%10>b:
+       b=a%10
+    else:
+        b=b
+    a = a//10
+print(b)
+
+#Задача 10
+
+b=0
+a = 523454
+while a>0:
+    if a%10 == 5:
+        b=b+1
+    else:
+        b=b
+    a=a//10
+print(b)
+'''
+
+
